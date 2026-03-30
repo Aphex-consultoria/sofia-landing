be8364d161e4b25015d35d757180cf792a5f273a
Thiago Alves <contato@aphex.com.br>
2026-03-29 22:14:18 -0300

    quarta alteração

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/termos', (req, res) => {
 });
 
 app.get('*', (req, res) => {
+  // Não devolver index.html para arquivos estáticos inexistentes (evita HTML no lugar da imagem)
+  if (path.extname(req.path)) {
+    return res.status(404).end();
+  }
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
